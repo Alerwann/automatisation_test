9191cd21ae39633425e641addaa90189f6dd4245
Alerwann <alerwann411@gmail.com>
2025-11-30 15:07:50 +0100

    feat: Implement Flutter MethodChannel for native screen automation and update application bundle identifiers across platforms.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.automatisation_test"
+    namespace = "com.alerwann.automatisation_test"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.automatisation_test"
+        applicationId = "com.alerwann.automatisation_test"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/automatisation_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/automatisation_test/MainActivity.kt
@@ -1,5 +1,35 @@
-package com.example.automatisation_test
+package com.alerwann.automatisation_test
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+import androidx.annotation.NonNull
+
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity: FlutterActivity() {
+
+    private val CHANNEL = "com.alerwann/screen_automation" 
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            
+            when (call.method) {
+                "startScreenCapture" -> {
+                    // TODO: Étape 4 - Logique de capture d'écran (MediaProjection)
+                    result.success(true) // On renvoie une réponse rapide pour l'instant
+                }
+                "getAppList" -> {
+                    // TODO: Étape 5 - Logique pour récupérer la liste des applications installées
+                    result.success(emptyList<String>())
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+}
